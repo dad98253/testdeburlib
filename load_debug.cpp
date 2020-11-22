a051a016443588e86e3031e137b5cff3ced90930
john kuras <w7og@yahoo.com>
2020-11-22 00:20:00 -0800

    fixed Release compile to exclude debuglib stuff

--- load_debug.cpp
+++ load_debug.cpp
@@ -9,8 +9,6 @@
 #pragma message( "Compiling " __FILE__ " on " __DATE__ " at " __TIME__ )
 #pragma message( "File last modified on " __TIMESTAMP__ )
 #pragma message( "  ")
-#pragma title( "debuglib version 2.0" )
-#pragma subtitle( "Copyright (c) 2018, Nehemiah Ministries, Inc." )
 #pragma comment( compiler )
 #pragma comment( user, "File: " __FILE__ ". Compiled on " __DATE__ " at " __TIME__ ".  Last modified on " __TIMESTAMP__ )
 #endif
@@ -26,6 +24,7 @@ extern DEBUGOPTIONSTYPE myoptions;
 
 void load_debug () 
 {
+#ifdef DEBUG
 	struct list_entry *current;
 	int i;
 	int j=0;
@@ -149,5 +148,6 @@ void load_debug ()
 		if ( bdebug_flag_set[QUIET] ) debugVirbosity = 0;
 
 	}
+#endif	// DEBUG
 	return;
 }
